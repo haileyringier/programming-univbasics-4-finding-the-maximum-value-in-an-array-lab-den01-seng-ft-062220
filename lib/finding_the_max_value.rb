5eb81c3cb754bdd6ee95702616a10b78cb52649b
Hailey Ringier <haileyringier@outlook.com>
2020-06-13 02:59:56 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,12 @@
 def find_max_value(array)
   counter = 0 
+  current_max = 0
   while counter < array.length do 
-  
+    if array[counter] > current_max then
+      current_max = array[counter]
+    end
     counter += 1 
   end
-  return 
+  return current_max 
 end
 
-# arry.sort! 
-# return array[-1]
